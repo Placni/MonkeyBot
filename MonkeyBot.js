228682d3d52655a190af43b39067dc383646b461
Placni <pat.lacni@yahoo.com>
2021-05-04 17:08:52 -0500

    functional mongoDB connection

--- MonkeyBot.js
+++ MonkeyBot.js
@@ -1,11 +1,13 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const mongo = require('./util/mongo.js');
 const colors = require('colors');
 const fs = require('fs');
 require('log-timestamp');
 require('dotenv').config();
+const mongoose = require('mongoose');
 
 const prefix = process.env.PREFIX;
+const client = new Discord.Client();
 
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js')); 
@@ -14,8 +16,11 @@ for(const file of commandFiles){
     client.commands.set(command.name, command);
 }
 
-client.once('ready', () => {
+client.once('ready', async () => {
     console.log('MonkeyBot Online' .magenta);
+    await mongo().then(mongoose => {
+        console.log('Connected to Mongo' .green);
+    })
 });
 
 client.on('message', message =>{
---
+++ util/mongo.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+module.exports = async () => {
+    await mongoose.connect(process.env.MONGODB_SRV, {
+      keepAlive: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    return mongoose;
+  };
