3ab333d3c20a1e8606517a46159050b6450ea4fe
Placni <pat.lacni@yahoo.com>
2022-08-31 14:50:39 -0500

    Added slash functionality

--- commands/fun/deafcheck.js
+++ commands/fun/deafcheck.js
@@ -1,29 +1,46 @@
-const common = require('@util/common');
-const Discord = require('discord.js');
+const { findVC } = require('@util/common');
 
 module.exports = {
     name: "deafcheck",
-    description: "moves deafened users out of the vc",
+    description: "Moves deafened users out of the vc",
     usage: `\`${process.env.PREFIX}deafcheck <channel>\``,
     alias: ["deaf"],
     disabled: false,
+    slash: true,
+    options: [
+        {
+            name: 'channel',
+            type: 'CHANNEL',
+            channelTypes: ['GUILD_VOICE'],
+            description: 'Channel to remove users from',
+            required: true
+        }
+    ],
     permission: ['MOVE_MEMBERS'],
-    execute(message, args){ 
-        if(!args[0]) return message.reply(" specify a channel you monkey");
-
-        let targetChannel = common.findVC(args.shift(), message);
-        if(!targetChannel) return message.reply(" couldn't find target channel!");
-        if(targetChannel.members.size == 0) return message.reply(" the target channel must have users in it!");
-
-        let i = 0
-        let channel = common.findVC('afk', message);
-        if(!channel) channel = null;
-        for(let [snowflake, guildMember] of targetChannel.members){
-            if(guildMember.voice.selfDeaf){
-                guildMember.voice.setChannel(channel);
-                message.channel.send(`**${guildMember.user.tag}** was removed from vc for being deafened`);
-                guildMember.send(`**${message.author.tag}** removed you from vc for being deafened`);
-                i++;
+    async execute(interaction, args){ 
+        const isSlash = interaction.isCommand?.();
+        if(isSlash){
+            let targetVC = await findVC(interaction.options.getChannel('channel'), interaction);
+            if(targetVC) return interaction.reply({content: 'An error has occured', ephemeral: true});
+            let moved = deafCheck(targetVC);
+            return interaction.reply({ content: `Removed ${moved} members from ${targetVC.name}`, ephemeral: true });
+        } else {
+            if(!args.length) return interaction.reply({ content: 'Specify a voice channel!'});
+            let targetVC = findVC(args[0], interaction);
+            if(!targetVC) return interaction.reply({ content: 'Error finding specified voice channel!' });
+            let moved = deafCheck(targetVC);
+            return interaction.reply({ content: `Removed ${moved} users from ${targetVC.name}` });
+        }
+        function deafCheck(targetVC) {
+            let i;
+            let channel = (!common.findVC('afk', interaction)) ? common.findVC('afk', interaction) : null;
+            for(let [snowflake, guildMember] of targetVC.members){
+                if(guildMember.voice.selfDeaf){
+                    guildMember.voice.setChannel(channel);
+                    guildMember.send(`**${interaction.member.user.tag}** removed you from vc for being deafened`);
+                    i++;
+                }
+            return i;
             }
         }
     }
