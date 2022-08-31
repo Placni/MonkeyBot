514a5e41e28ac41cee77b8af0c95f23e0ec2b363
Placni <pat.lacni@yahoo.com>
2022-08-30 20:57:31 -0500

    Updated with slash functionality

--- commands/guild/user.js
+++ commands/guild/user.js
@@ -1,43 +1,57 @@
-const dbhelper = require('@util/dbhelper');
-const { findMember } = require('@util/common');
-const Discord = require('discord.js');
+const { getGuildUserProfile } = require('@util/dbhelper');
+const { findMember, hhmmss } = require('@util/common');
+const { MessageEmbed } = require('discord.js');
 const moment = require('moment');
 
 module.exports = {
     name: "user",
-    description: "display user information",
+    description: "Displays user information",
     usage: `\`${process.env.PREFIX}user <target>\``,
-    alias: [],
     disabled: false,
-    permission: ['ADMINISTRATOR'],
-    async execute(message, args, client){ 
-        let guildMember;
-        if(args[0]){
-            guildMember = await findMember(args[0], message);
-            if(!guildMember) return message.channel.send(`Couldn't find desired user!`); 
-        } else guildMember = message.member;
-        let userdata = await dbhelper.getGuildUserProfile(message.guild.id, guildMember.id);
-
-        let nicknames = !userdata.nicknames ? ["No Nicknames Logged"] : userdata.nicknames;
-        let vcTime = !userdata.vcTime ? 'None' : userdata.vcTime;
-        if (vcTime !== 'None'){
-            let hrs = Math.floor((vcTime / 60) / 60);
-            let mins = Math.floor((vcTime / 60) - (hrs * 60));
-            let secs = Math.floor(vcTime - ((hrs * 60 * 60) + (mins * 60)));
-            vcTime = `${hrs}hrs ${mins}mins ${secs}s`;
+    slash: true,
+    options: [
+        {
+            name: 'user',
+            type: 'USER',
+            description: 'get the info of this user',
+            required: false
         }
-        let msgCount = !userdata.msgCount ? 'None' : userdata.msgCount;
-        
-        const embed = new Discord.MessageEmbed()
-            .setColor(guildMember.displayHexColor)
-            .addFields(
-                {name: 'User Info', value: `Joined Server: \`${moment(guildMember.joinedAt).format("MM/d/YYYY, h:mm:ss a")}\`\nCreated on: \`${moment(guildMember.user.createdAt).format("MM/d/YYYY, h:mm:ss a")}\`\nUser ID: \`${guildMember.id}\``},
-                {name: 'Past Nicknames', value: `${nicknames.join(', ')}`},
-                {name: 'Interaction', value: `Time in VC: \`${vcTime}\`\nMessages Sent: \`${msgCount}\``}
-            )
-            .setDescription(`<@${guildMember.id}> (${guildMember.user.tag})`)
-            .setThumbnail(guildMember.user.displayAvatarURL({ format: "png", dynamic: true, size: 2048 }))
-        
-        message.channel.send({embeds: [embed]});
+    ],
+    async execute(interaction, args){ 
+        const isSlash = interaction.isCommand?.();
+        let guildMember;
+
+        if (isSlash){
+            guildMember = await findMember(interaction.options.getUser('user'), interaction);
+            if(!guildMember) guildMember = interaction.member
+            let finalEmbed = buildEmbed(guildMember);
+            return interaction.reply({embeds: [finalEmbed]})
+        } else {
+            if(args[0]){
+                guildMember = await findMember(args[0], interaction);
+                if(!guildMember) return interaction.reply(`Couldn't find desired user!`); 
+            } else guildMember = interaction.member;
+            let finalEmbed = buildEmbed(guildMember);
+            return interaction.reply({embeds: [finalEmbed]});
+        }
+
+        function buildEmbed(member){
+            let userdata = await getGuildUserProfile(interaction.guildId, member.id)
+            let nicknames = !userdata.nicknames ? ["No Nicknames Logged"] : userdata.nicknames;
+            let vcTime = !userdata.vcTime ? 'None' : userdata.vcTime;
+            if (vcTime !== 'None') vcTime = hhmmss(vcTime);
+            let msgCount = !userdata.msgCount ? 'None' : userdata.msgCount;
+
+            const userEmbed = new MessageEmbed()
+                .setColor(member.displayHexColor)
+                .addFields(
+                    {name: 'User Info', value: `Joined Server: \`${moment(member.joinedAt).format("MM/d/YYYY, h:mm:ss a")}\`\nCreated on: \`${moment(member.user.createdAt).format("MM/d/YYYY, h:mm:ss a")}\`\nUser ID: \`${member.id}\``},
+                    {name: 'Past Nicknames', value: `${nicknames.join(', ')}`},
+                    {name: 'Interaction', value: `Time in VC: \`${vcTime}\`\nMessages Sent: \`${msgCount}\``}
+                )
+                .setDescription(`<@${member.id}> (${member.user.tag})`)
+                .setThumbnail(member.user.displayAvatarURL({ format: "png", dynamic: true, size: 2048 }))
+            return userEmbed;
+        }
     }
 }
--- commands/utility/info.js
+++ commands/utility/info.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const moment = require('moment');
+const { hhmmss } = require('@util/common');
 
 module.exports = {
     name: "info",
@@ -7,26 +7,24 @@ module.exports = {
     usage: `\`${process.env.PREFIX}info\``,
     alias: [],
     disabled: false,
-    async execute(message, args, client){ 
+    slash: true,
+    async execute(interaction, args, client) {
+        const isSlash = interaction.isCommand?.();
+        let uptime = hhmmss((Date.now() - client.readyAt) / 1000);
 
-        let uptime = (Date.now() - client.readyAt) / 1000;
-        let hrs = Math.floor((uptime / 60) / 60);
-        let mins = Math.floor((uptime / 60) - (hrs * 60));
-        let secs = Math.floor(uptime - ((hrs * 60 * 60) + (mins * 60)));
-        uptime = `${hrs}hrs ${mins}mins ${secs}s`;
+        if (!isSlash) {
+            reply = await interaction.reply('Resolving...');
+            reply.edit({ content: null, embeds: [createEmbed(Math.round((reply.createdTimestamp - interaction.createdTimestamp)))] });
+        } else {
+            reply = await interaction.deferReply({ fetchReply: true });
+            interaction.editReply({ embeds: [createEmbed(Math.round((reply.createdTimestamp - interaction.createdTimestamp)))] });
+        }
 
-        message.channel.send('Resolving').then(msg => {
-            msg.delete();
-            message.channel.send({embeds: [createEmbed(msg.createdTimestamp - message.createdTimestamp)]});
-        })
-
-        function createEmbed(ping){
+        function createEmbed(ping) {
             const embed = new Discord.MessageEmbed()
-            .setColor('#803d8f')
-            .setAuthor(`Monkeybot`, client.user.avatarURL())
-            .addFields(
-                {name: 'Info', value: `Users: \`${client.users.cache.size}\`\nGuilds: \`${client.guilds.cache.size}\`\nUptime: \`${uptime}\`\nPing: \`${ping}ms\`\nAPI Latency: \`${client.ws.ping}ms\``},
-                )
+                .setColor('#803d8f')
+                .setAuthor(`Monkeybot`, client.user.avatarURL())
+                .addFields({ name: 'Info', value: `Users: \`${client.users.cache.size}\`\nGuilds: \`${client.guilds.cache.size}\`\nUptime: \`${uptime}\`\nPing: \`${ping}ms\`\nAPI Latency: \`${client.ws.ping}ms\`` })
             return embed;
         }
     }
