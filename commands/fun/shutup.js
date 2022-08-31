c41149feb4323a5da70df9cc5fd6ae86d2c434ec
Placni <pat.lacni@yahoo.com>
2022-08-31 15:01:21 -0500

    Added slash functionality

--- commands/fun/shutup.js
+++ commands/fun/shutup.js
@@ -1,71 +1,67 @@
-const common = require('@util/common');
-const Discord = require('discord.js');
-
-//THIS COMMAND WILL RATE LIMIT THE BOT IF USED IN A CHANNEL WITH OVER 5 PEOPLE
+const { findMember } = require('@util/common');
 
 module.exports = {
     name: "shutup",
-    description: "server mutes the targeted user(s)",
-    usage: `\`${process.env.PREFIX}shutup <target> <channel> <time>\``,
+    description: "Server mutes the target user",
+    usage: `\`${process.env.PREFIX}shutup <target> <time>\``,
     alias: ["silence"],
     disabled: false,
+    slash: true,
+    options: [
+        {
+            name: 'target',
+            type: 'USER',
+            description: 'user to be silenced',
+            require: true
+        },
+        {
+            name: 'time',
+            type: 'INTEGER',
+            description: 'how long the user will be silenced in seconds',
+            require: true,
+            min_value: 1,
+            max_value: 10,
+        }
+    ],
     permission: ['MUTE_MEMBERS'],
-    async execute(message, args){ 
-        let targetChannel;
-        let target = args.shift();
+    async execute(interaction, args){
+        const isSlash = interaction.isCommand?.();
 
-        if (!target) return message.reply("Specify a person you monkey");
-        if (target == 'all'){
-            targetChannel = args.shift();
-            targetChannel = common.findVC(targetChannel, message);
-            if(!targetChannel) return message.reply("Couldn't find desired channel!"); 
-            target = 'all'
+        if(isSlash) {
+            let target = await findMember(interaction.options.getUser('target'), interaction);
+            if(!target || !target.voice.channel) return interaction.reply({ content: `Couldn't find that user!`, ephemeral: true });
+            let time = await interaction.options.getInteger('time');
+            if(!time) time = 5;
+            let success = plsShutUp(target, (time * 1000));
+            if(!success) return interaction.reply({ content: `An error occured while muting your target!`, ephemeral: true });
+            return interaction.reply({ content: `Muted <@${target.id}> for ${time}s`, ephemeral: true });
         } else {
-            target = await common.findMember(target, message);
-            if (!target) return message.reply("Couldn't find desired user!");               
+            if(!args.length) return interaction.reply({ content: 'Specify a user!' });
+            let target = await findMember(args[0], interaction);
+            if(!target) return interaction.reply({ content: `Couldn't find specified user!` });
+            let time = parseInt(args[1]);
+            (!time || time > 10 || time < 1) ? time = 5 : time = Math.ceil(time);
+            let success = plsShutUp(target, (time * 1000));
+            if(!success) return interaction.reply({ content: `An error occured while muting your target!` });
+            return interaction.reply({ content: `Muted <@${target.id}> for ${time}s` });
         }
 
-        let time = parseInt(args.shift());
-        if (!time || time > 10){
-            time = 5000;
-        } else {
-            time = Math.ceil(time);
-            time = (time * 1000);
-        }
-
-        function ToggleMute(target){
-            if (!target.voice.channel){
-                message.reply("That user isn't in vc you monkey");
-                return false;
-            }
-            if (!target.voice.serverMute){
-                target.voice.setMute(true, "");
-            } else {
-                target.voice.setMute(false, "");
-            }
+        function toggleMute(target){
+            if (!target.voice.channel) return false;
+            (!target.voice.serverMute) ? target.voice.setMute(true, "") : target.voice.setMute(false, "");
             return true;
         }
         function sleep(ms) {
             return new Promise((resolve) => {
               setTimeout(resolve, ms);
             });
-          }
-        async function PlsShutUp(target, ms) {
-            let done = ToggleMute(target);
-            if (!done) return;
-            await sleep(ms);
-            ToggleMute(target);
         }
-
-        //mutes user(s) based on target type
-        if(target == 'all'){
-            for(let [snowflake, guildMember] of targetChannel.members){
-                PlsShutUp(guildMember, time);
-                message.channel.send(`**${guildMember.user.tag}** has been silenced for **${(time / 1000)}s**`);
-            }
-          } else {
-            PlsShutUp(target, time);
-            message.channel.send(`**${target.user.tag}** has been silenced for **${(time / 1000)}s**`);
-        } 
+        async function plsShutUp(target, ms) {
+            let done = toggleMute(target);
+            if (!done) return false;
+            await sleep(ms);
+            toggleMute(target);
+            return true;
+        }
     }
 }
