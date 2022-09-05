b7cc23aa0d84b0c0d3bf3d99708b9885414c31d6
Placni <pat.lacni@yahoo.com>
2022-09-04 22:35:01 -0500

    Fixed deprecation warning

--- util/mongo.js
+++ util/mongo.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 
 module.exports = async () => {
     await mongoose.connect(process.env.MONGODB_SRV, {
-      keepAlive: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+        keepAlive: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
     });
     return mongoose;
-  };
+};
